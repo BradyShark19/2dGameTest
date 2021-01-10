c7cf025e4f2a72a3bef1f3edacfb77a370ffe076
raulBrady <bradyraule@gmail.com>
2021-01-09 21:00:58 -0500

    animacion de bebe

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Renderer fondo;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.015f, 0) * Time.deltaTime;
+    }
+}
