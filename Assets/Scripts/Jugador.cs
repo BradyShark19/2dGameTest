0f7e54ceaf48a041c194bdf67cfa60f37e8e37a1
raulBrady <bradyraule@gmail.com>
2021-01-10 18:26:08 -0500

    version 1 finalizado

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float fuerzaSalto;
+    public GameManager gameManager;
+
+
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+
+    void Start()
+    {
+        animator    = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJumping", true);
+            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Suelo") 
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+        if (collision.gameObject.tag == "Obstaculo")
+        {
+            gameManager.gameOver = true; 
+        }
+    }
+
+}
+
